29c69fc183940ba94baf018eea47c919f7eebebe
harshwardhan27 <gupta,wardhanharsh@gmail.com>
2018-09-28 01:41:26 +0530

    Modified messaging package and add driver class into it.

--- Brightside_Edited_Code/mentoring/messaging.java
+++ Brightside_Edited_Code/mentoring/messaging.java
@@ -1,36 +1,65 @@
 package mentoring;
-import brightsideDefault.homePage;
 import utility.Constant;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
-public class messaging extends homePage {
+
+public class messaging {
+  
 	
-   @Test
-   public void mesaging() throws InterruptedException{
-	try {
-		driver.get(Constant.baseURL);
-		driver.findElement(By.linkText("Sign in")).click();
-		driver.findElement(By.xpath("/html/body/app/main/sign-in/div/div/div/div/form/div[1]/div/input")).sendKeys(Constant.newEmailId);
-		driver.findElement(By.xpath("/html/body/app/main/sign-in/div/div/div/div/form/div[2]/div/input")).sendKeys(Constant.newPassword);
-		driver.findElement(By.xpath("/html/body/app/main/sign-in/div/div/div/div/form/div[3]/input")).click();
-		Thread.sleep(3000);
-		String mentoringURL = driver.getCurrentUrl();
-		if (mentoringURL.equalsIgnoreCase(Constant.messagingURL)){
-			System.out.println("Channels URL Matched");
-			driver.findElement(By.linkText("Mentoring")).click();
-			driver.findElement(By.linkText("Mentoring")).click();
-			driver.findElement(By.id("chat__input")).sendKeys(Constant.testMessage);
+	public WebDriver driver;
+	  
+	  @BeforeTest
+	  public void invokeBrowser(){
+		try {
+			System.setProperty("webdriver.chrome.driver","C:\\Users\\HARSH\\workspace\\MySeleniumCode\\src\\org\\brightside\\chromedriver.exe");
+//			System.setProperty("webdriver.chrome.driver","D:\\Selenium\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			driver.get(Constant.baseURL);	
+		} catch (Exception e) {
+			e.printStackTrace();
+		   }
+	  }
+	  
+	   @Test(priority=0)
+	   public void mesaging() throws InterruptedException{
+		try {
+			driver.get(Constant.baseURL);
+			driver.findElement(By.linkText("Sign in")).click();
+			driver.findElement(By.xpath("/html/body/app/main/sign-in/div/div/div/div/form/div[1]/div/input")).sendKeys(Constant.newEmailId);
+			driver.findElement(By.xpath("/html/body/app/main/sign-in/div/div/div/div/form/div[2]/div/input")).sendKeys(Constant.newPassword);
+			driver.findElement(By.xpath("/html/body/app/main/sign-in/div/div/div/div/form/div[3]/input")).click();
 			Thread.sleep(3000);
-			driver.findElement(By.id("chat__send-message")).click();
-			System.out.println(Constant.testMessage + " " + "message to be sent");		
-		} else {
-		    System.out.print("Channels URL doesn't matched");
-		    driver.get(Constant.baseURL + "/signout");
-		    driver.close();
-		 	}
-	 } catch (Exception e) {
-		e.printStackTrace();
-		}
-   }	
+			driver.findElement(By.xpath("/html/body/app/main/pages/div/div/div/feeds/div/div[1]/div/div[1]/a")).click();
+			Thread.sleep(3000);
+			String mentoringURL = driver.getCurrentUrl();
+			if (mentoringURL.equalsIgnoreCase(Constant.messagingURL)){
+				System.out.println("Channels URL Matched");
+				driver.findElement(By.linkText("Mentoring")).click();
+				driver.findElement(By.id("chat__input")).sendKeys(Constant.testMessage);
+				WebElement textbox = driver.findElement(By.id("chat__input"));
+				textbox.sendKeys(Keys.SPACE);
+				driver.findElement(By.id("chat__send-message")).click();
+				System.out.println(Constant.testMessage + " " + "message is sent");
+				
+			} else {
+			    System.out.print("Channels URL doesn't matched");
+			    driver.get(Constant.baseURL + "/signout");
+			    driver.close();
+			 	}
+		 } catch (Exception e) {
+			e.printStackTrace();
+			}
+	  }	
 }
--- Brightside_Edited_Code/utility/Constant.java
+++ Brightside_Edited_Code/utility/Constant.java
@@ -1,7 +1,7 @@
 package utility;
 
 public class Constant {
-	
+  
 	//Constant URL
 	 public static final String baseURL = "http://dev.angular.brightsidementoring.tudip.uk";
 	 public static final String signUpURL = "http://dev.angular.brightsidementoring.tudip.uk/sign-up?project_code=";
@@ -14,7 +14,7 @@ public class Constant {
 	 public static final String projectCode = "HCLOHR";
 	 public static final String newfirstName = "Demo";
 	 public static final String newlastName = "User";
-	 public static final String newEmailId = "demo.email@tudip.com"; 
+	 public static final String newEmailId = "johnny.lamonica+email123456@tudip.com"; 
 	 public static final String newPassword = "tudip123";
 	 public static final String newPostCode = "AB99AB";
 	 public static final String newDate = "27";
@@ -24,5 +24,5 @@ public class Constant {
 	 //Messaging Constant Variables
 	 public static final String messagingURL = "http://dev.angular.brightsidementoring.tudip.uk/channels";
 	 public static final String testMessage = "Test Message";
-
+  
 }
