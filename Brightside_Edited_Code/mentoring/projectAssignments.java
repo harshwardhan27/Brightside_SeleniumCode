8c77a074f1b7f67cfcfc7003265b93797336bd0f
harshwardhan27 <gupta.wardhanharsh@gmail.com>
2018-10-25 17:46:16 +0530

    Updated the test script for assigment page.

--- Brightside_Edited_Code/mentoring/projectAssignments.java
+++ Brightside_Edited_Code/mentoring/projectAssignments.java
@@ -1,12 +1,16 @@
 package mentoring;
 import utility.Constant;
+import brightsideDefault.homePage;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import brightsideDefault.homePage;
+import org.testng.annotations.*;
+import java.time.LocalDateTime;  
 
 public class projectAssignments extends homePage{
 	
+	@Test(priority=0)
 	public void testUploadAssignments() throws InterruptedException{
 		
 		//User Login
@@ -14,7 +18,8 @@ public class projectAssignments extends homePage{
 		obj_sign_in.user_login();
 		
 		//Upload Assignments
-		driver.findElement(By.xpath("//*[@id='mySidenavR']/ul/li[4]/a")).click();
+		WebElement wb_assignments = driver.findElement(By.id("//*[@id='mySidenavR']/ul/li[4]/a"));
+		wb_assignments.click();
 		driver.findElement(By.xpath("/html/body/app/main/pages/div/div/div/assignments/div/div/div[1]/div[2]/a")).sendKeys(Constant.assignmentFilePath);
 		driver.findElement(By.xpath("/html/body/app/main/pages/div/div/div/assignments/div/div/div[1]/div[3]/div[contains(text(),Constant.assignmentFileName)]")).isDisplayed();
 		System.out.println("File has been selected");
@@ -28,6 +33,28 @@ public class projectAssignments extends homePage{
 			System.out.println("User details doesn't updated successfully");
 		}
 		System.out.println(Constant.uploadConfirmMessage);	
-	}
+		}
 	
+	@Test(priority=1)
+	public void checkUploadFileName() throws InterruptedException{
+		//check uploaded date
+		LocalDateTime now = LocalDateTime.now();
+	    int year = now.getYear();
+	    int month = now.getMonthValue();
+	    int day = now.getDayOfMonth();
+	    String currnetDate = month + day + ", " + year;
+	    System.out.println(currnetDate);
+	    
+	    //check uploaded file name
+		try{
+			Assert.assertFalse(driver.findElements(By.xpath("/html/body/app/main/pages/div/div/div/assignments/div/div/div[2]/div/table/tbody/tr[2]/td[2]")).contains(Constant.assignmentFileName));
+		}catch(AssertionError e){
+			System.out.println("File doesn't uploaded");
+		}
+		System.out.println("File uploaded successfully");
+		
+		//check uploaded file download
+		driver.findElement(By.xpath("/html/body/app/main/pages/div/div/div/assignments/div/div/div[2]/div/table/tbody/tr[2]/td[3]/a")).click();
+		System.out.println("Uploaded file has been downloaded");
+	}
 }
